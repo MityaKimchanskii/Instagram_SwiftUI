046e5f5bc7938db4f698ada1915308bf1784474f
Dmitrii Kim <mitya_kim@icloud.com>
2023-08-02 21:48:08 -0700

    feat: Add Instagram project

---
+++ Instagram_Clone/Instagram_Clone/App/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram_Clone
+//
+//  Created by Mitya Kim on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Instagram_Clone/Instagram_Clone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabView.swift
+//  Instagram_Clone
+//
+//  Created by Mitya Kim on 8/2/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("feed")
+            
+            Text("feed")
+            
+            Text("feed")
+            
+            Text("feed")
+            
+            Text("feed")
+            
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
